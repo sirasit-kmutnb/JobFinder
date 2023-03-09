71904985bee4b01ade89550011e71e4e3443e8f0
sirasit-kmutnb <s6401012620111@email.kmutnb.ac.th>
2023-03-10 01:13:40 +0700

    .

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+            <div className="collapse navbar-collapse justify-content-between" id="navbarNavDropdown">
                 <ul className="navbar-nav">
                     <li className="nav-item">
                     <Link className="nav-link" aria-current="page" to="/">Home</Link>
@@ -23,6 +23,14 @@ const Navbar = () => {
                     <li className="nav-item">
                     <Link className="nav-link" to="/Post">Single Page</Link>
                     </li>
+                </ul>
+                <ul className="navbar-nav">
+                  <li className="navbar-nav">
+                    <Link className="nav-link text-success" to="#">Login</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="btn btn-primary" to="#">Register</Link>
+                  </li>
                 </ul>
             </div>
         </div>
