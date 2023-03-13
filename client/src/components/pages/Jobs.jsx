67933d946a655c5d65bad645f1f6e28a0d26125f
sirasit-kmutnb <s6401012620111@email.kmutnb.ac.th>
2023-03-13 22:27:55 +0700

    .

--- client/src/components/pages/Jobs.jsx
+++ client/src/components/pages/Jobs.jsx
@@ -6,7 +6,9 @@ import { useEffect, useState } from "react";
 
 const Jobs = () => {
   const [posts, setPosts] = useState([]);
+  const [showPosts, setShowposts] = useState([]);
   const [usernames, setUsernames] = useState([]);
+  const [input, setInput] = useState("");
 
   const findUsername = (accountID) => {
     axios
@@ -21,11 +23,12 @@ const Jobs = () => {
       })
       .catch((err) => alert(err));
   };
+
   const fetchData = () => {
     axios
       .get(`${import.meta.env.VITE_APP_API}/posts`)
       .then((response) => {
-        // console.log(response.data)
+        console.log(response.data);
         setPosts(response.data);
       })
       .catch((err) => alert(err));
@@ -35,29 +38,86 @@ const Jobs = () => {
     fetchData();
   }, []);
 
+  const handleSearch = (e) => {
+    // condition that add the post that parallel with keyword to Showposts list
+    if (posts != null) {
+      const Datalength = posts.length;
+      console.log(Datalength);
+      let nodata = true; //
+
+      for (let i = 0; i < Datalength; i++) {
+        // check if input include some word in data
+        if (
+          posts[i].title.toLowerCase().includes(input.toLowerCase()) ||
+          posts[i].details.toLowerCase().includes(input.toLowerCase()) // ||
+          // findUsername(posts[i].author_id).includes(input.toLowerCase()) ///don't know how to fix it
+        ) {
+          // work on the first time setShowdata
+          if (nodata) {
+            // console.log("trips: " + nodata);
+            setShowposts([posts[i]]);
+            nodata = false;
+          } else {
+            // work on the second of time setShowdata
+            setShowposts((prev) => [...prev, posts[i]]);
+          }
+        }
+        if (nodata) {
+          // if input not include any word in data
+          setShowposts();
+          console.log(showPosts);
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, [posts]);
+
   return (
     <AnimatedPage>
-      {posts.map((post, index) => (
-        <div
-          className="row mt-3"
-          key={index}
-          style={{ borderBottom: "1px solid silver" }}
-        >
-          <div className="col pt-3 pb-2">
-            <Link to={`/Jobs/${post.slug}`}>
-              <h3>{post.title}</h3>
-            </Link>
-            <div className="pt-3">{post.details.substring(0, 300)}</div>
-            {/* <div className="pt-3">{post.details.info2.substring(0, 300)}</div> */}
-            {/* <div className="pt-3">{post.details.info2.substring(0, 300)}</div> */}
-            <p className="text-muted">
-              ผู้โพสต์:{" "}
-              {usernames[post.author_id] || findUsername(post.author_id)} ,
-              เผยแพร่ : {new Date(post.createdAt).toLocaleString()}
-            </p>
+      <div className="search__container">
+        <form onSubmit={(e) => handleSearch(e.preventDefault())}>
+          <input
+            id="keyword"
+            name="keyword"
+            className="search__input"
+            type="text"
+            placeholder="Search"
+            autoComplete="off"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          ></input>
+        </form>
+      </div>
+      {showPosts ? (
+        showPosts.map((post, index) => (
+          <div
+            className="row mt-3"
+            key={index}
+            style={{ borderBottom: "1px solid silver" }}
+          >
+            <div className="col pt-3 pb-2">
+              <Link to={`/Jobs/${post.slug}`}>
+                <h3>{post.title}</h3>
+              </Link>
+              <div className="pt-3">{post.details.substring(0, 300)}</div>
+              {/* <div className="pt-3">{post.details.info2.substring(0, 300)}</div> */}
+              {/* <div className="pt-3">{post.details.info2.substring(0, 300)}</div> */}
+              <p className="text-muted">
+                ผู้โพสต์:{" "}
+                {usernames[post.author_id] || findUsername(post.author_id)} ,{" "}
+                เผยแพร่ : {new Date(post.createdAt).toLocaleString()}
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <>
+          <h3 className="notfound">Not found</h3>
+        </>
+      )}
     </AnimatedPage>
   );
 };
